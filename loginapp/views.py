18f4d1f7f808518067fabbc3c0ff18eb0e9fdb68
Jickson007 <Jicksonjose13@gmail.com>
2023-11-26 20:46:55 +0530

    created  register api

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from loginapp.models import PostModel
+from loginapp.models import UserModel
 import json
 from loginapp.serializer import *
 from django.db.models import Q
 # Create your views here.
 
 
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        receieved_data = json.loads(request.body)
+        print(receieved_data)
+        serialized_data = UserSerializer(data=receieved_data)
+        print("read serialized data")
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return HttpResponse(json.dumps({"status": "registered successfully"}))
+        else:
+            return HttpResponse(json.dumps({"status": "registered failed"}))
