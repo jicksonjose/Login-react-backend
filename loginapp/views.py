8e86b67a17f218949c35030d4be54ec095677dc5
Jickson007 <Jicksonjose13@gmail.com>
2023-11-26 22:41:42 +0530

    created login api

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from loginapp.models import PostModel
+from loginapp.models import UserModel
 import json
 from loginapp.serializer import *
 from django.db.models import Q
 # Create your views here.
 
 
+@csrf_exempt
+def loginApi(request):
+    if request.method == 'POST':
+        receieved_data = json.loads(request.body)
+        print(receieved_data)
+        getemail = receieved_data["email"]
+        getpassword = receieved_data["password"]
+        data = list(UserModel.objects.filter(Q(email__exact=getemail) & Q(password__exact=getpassword)).values())
+        return HttpResponse(json.dumps(data)) 
+    else:
+        return HttpResponse(json.dumps({"status": "login details Invalid"}))  
