e22c4e751e5eb7f3e5ea03d3107c2d3ecd147719
Jickson007 <Jicksonjose13@gmail.com>
2023-11-28 15:31:33 +0530

    api folder set up for auth and register

---
+++ api/v1/register/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from loginapp.models import UserModel
+import json
+from .serializer import *
+from django.db.models import Q
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        receieved_data = json.loads(request.body)
+        print(receieved_data)
+        serialized_data  = UserSerializer(data=receieved_data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return HttpResponse(json.dumps({"status": "Registered successfully"}))    
+        else:
+            return HttpResponse(json.dumps({"status": "error"}))   
