9bd392367ba90b5c7f830811227b54574e34dae4
Jickson007 <Jicksonjose13@gmail.com>
2023-11-28 14:19:51 +0530

    register

--- loginapp/views.py
+++ loginapp/views.py
@@ -8,3 +8,15 @@ from django.db.models import Q
 # Create your views here.
 
 
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        receieved_data = json.loads(request.body)
+        print(receieved_data)
+        serialized_data  = UserSerializer(data=receieved_data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return HttpResponse(json.dumps({"status": "Registered successfully"}))    
+        else:
+            return HttpResponse(json.dumps({"status": "error"}))   
